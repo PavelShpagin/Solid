f6037ff23fea2b41d585ee3d97aea6ef8a8d67b1
PavelShpagin <pavelandrewshpagin@gmail.com>
2023-11-27 10:34:07 +0200

    Add project files.

---
+++ Solid_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+//Lisco
+
+class Rectangle
+{
+	public int Width { get; set; }
+	public int Height { get; set; }
+
+	public int GetRectangleArea()
+	{
+		return Width * Height;
+	}
+}
+
+class Square
+{
+	public int Side { get; set; }
+	public int GetSquareArea()
+	{
+		return Side * Side;
+	}
+}
+class Program
+{
+	static void Main(string[] args)
+	{
+		Rectangle rect = new Rectangle();
+		rect.Width = 5;
+		rect.Height = 10;
+
+		Console.WriteLine(rect.GetRectangleArea());
+		Console.ReadKey();
+	}
+}
